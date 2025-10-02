0420c93ce9859b9958c25471bcbcdb79bb69813f
youzi <youri@saulx.com>
2025-10-02 16:50:26 +0200

    update imports

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,14 @@
-import { type BasedClient, Provider as BasedClientProvider } from "@based/sdk";
+import {
+  type BasedClient,
+  Provider as BasedClientProvider,
+  based,
+} from "@based/sdk";
 import { createRoot } from "react-dom/client";
 import { Counter } from "./components/counter.js";
 import "./index.css";
 
 import basedConfig from "../../based.js";
-export const based: BasedClient = client(basedConfig);
+export const client: BasedClient = based(basedConfig);
 const rootElement = document.getElementById("root")!;
 const root = createRoot(rootElement);
 
@@ -17,7 +21,7 @@ const App = () => {
 };
 
 root.render(
-  <BasedClientProvider client={based}>
+  <BasedClientProvider client={client}>
     <App />
   </BasedClientProvider>
 );
